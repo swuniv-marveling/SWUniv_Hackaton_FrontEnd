3caf451d16a4fc2cd0b7e79e9fed8694ba68a550
osohyun0224 <osohyun0224@naver.com>
2023-06-29 14:55:39 +0900

    feat: 병합 후 UI 수정

--- src/imageinput.jsx
+++ src/imageinput.jsx
@@ -74,29 +74,20 @@ const ImageUpload = () => {
           )}
         </label>
         {selectedImage && (
-          <div style={{ position: 'relative', width: '100%', height: '100%' }}>
-            <div
+          <div style={{ position: 'absolute', top: 0, bottom: 0, left: 0, right: 0 }}>
+            <img
+              src={selectedImage}
+              alt="Preview"
               style={{
                 position: 'absolute',
-                top: 0,
-                bottom: 0,
-                left: 0,
-                right: 0,
-                display: 'flex',
-                alignItems: 'center',
-                justifyContent: 'center',
+                top: '50%',
+                left: '50%',
+                transform: 'translate(-50%, -50%)',
+                maxWidth: '100%',
+                maxHeight: '100%',
+                objectFit: 'contain'
               }}
-            >
-              <img
-                src={selectedImage}
-                alt="Preview"
-                style={{
-                  width: '100%',
-                  height: '100%',
-                  objectFit: 'cover',
-                }}
-              />
-            </div>
+            />
           </div>
         )}
       </div>
